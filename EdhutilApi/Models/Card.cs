1405e68c6a588523074e8202532e8a7b2b9e8e6f
Jacob Slaton <jslaton@protonmail.com>
2024-04-08 06:33:14 -0700

    Update Field Names in Card Model to Match Database

--- EdhutilApi/Models/Card.cs
+++ EdhutilApi/Models/Card.cs
@@ -1,47 +1,50 @@
+using Dapper;
+
 namespace Edhutil.Api
 {
     public class Card
     {
-        public string? AsciiName { get; set; } = null;
-        public string BorderColor { get; set; } = string.Empty;
+        public string? Ascii_name { get; set; } = null;
+        public string Border_color { get; set; } = string.Empty;
         public string? Defense { get; set; } = null;
-        public int? EdhrecRank { get; set; } = null;
-        public int? EdhrecSaltiness { get; set; } = null;
-        public string? FaceFlavorName { get; set; } = null;
-        public double? FaceManaValue { get; set; } = null;
-        public string? FaceName { get; set; } = null;
-        public string? FlavorName { get; set; } = null;
-        public string? FlavorText { get; set; } = null;
-        public string? FrameVersion { get; set; } = null;
-        public bool HasAlternativeDeckLimit { get; set; } = false;
-        public bool HasFoil { get; set; } = false;
-        public bool HasNonFoil { get; set; } = false;
-        public bool IsAlternative { get; set; } = false;
-        public bool IsFullArt { get; set; } = false;
-        public bool IsFunny { get; set; } = false;
-        public bool IsOnlineOnly { get; set; } = false;
-        public bool IsOversized { get; set; } = false;
-        public bool IsPromo { get; set; } = false;
-        public bool IsRebalanced { get; set; } = false;
-        public bool IsReprint { get; set; } = false;
-        public bool IsReserved { get; set; }
-        public bool IsStorySpotlight { get; set; } = false;
-        public bool IsTextless { get; set; } = false;
-        public bool IsTimeshifted { get; set; } = false;
+        public int? Edhrec_rank { get; set; } = null;
+        public int? Edhrec_saltiness { get; set; } = null;
+        public string? Face_flavor_name { get; set; } = null;
+        public double? Face_mana_value { get; set; } = null;
+        public string? Face_name { get; set; } = null;
+        public string? Flavor_name { get; set; } = null;
+        public string? Flavor_text { get; set; } = null;
+        public string Frame_version { get; set; } = string.Empty;
+        public bool Has_alternative_deck_limit { get; set; } = false;
+        public bool Has_foil { get; set; } = false;
+        public bool Has_non_foil { get; set; } = false;
+        public bool Is_alternative { get; set; } = false;
+        public bool Is_full_art { get; set; } = false;
+        public bool Is_funny { get; set; } = false;
+        public bool Is_online_only { get; set; } = false;
+        public bool Is_oversized { get; set; } = false;
+        public bool Is_promo { get; set; } = false;
+        public bool Is_rebalanced { get; set; } = false;
+        public bool Is_reprint { get; set; } = false;
+        public bool Is_reserved { get; set; }
+        public bool Is_storySpotlight { get; set; } = false;
+        public bool Is_textless { get; set; } = false;
+        public bool Is_timeshifted { get; set; } = false;
         public string Language { get; set; } = string.Empty;
         public string Layout { get; set; } = string.Empty;
         public string? Loyalty { get; set; } = null;
-        public string? ManaCost { get; set; } = null;
-        public double ManaValue { get; set; } = 0.0;
+        public string? Mana_cost { get; set; } = null;
+        public double Mana_value { get; set; } = 0.0;
         public string Name { get; set; } = string.Empty;
         public string Number { get; set; } = string.Empty;
-        public string? OriginalReleaseDate { get; set; } = null;
-        public string? OriginalText { get; set; } = null;
-        public string? OriginalType { get; set; } = null;
+        public string? Original_release_date { get; set; } = null;
+        public string? Original_text { get; set; } = null;
+        public string? Original_type { get; set; } = null;
         public string? Power { get; set; } = null;
         public string Rarity { get; set; } = string.Empty;
-        public string? SecurityStamp { get; set; } = null;
-        public string SetCode { get; set; } = string.Empty;
+        public Guid Scryfall_id { get; set; } = Guid.Empty;
+        public string? Security_stamp { get; set; } = null;
+        public string Set_code { get; set; } = string.Empty;
         public string? Side { get; set; } = null;
         public string? Signature { get; set; } = null;
         public string? Text { get; set; } = null;
