df3591427f6e2f07a709a527f17a691a5ad8f0e4
Jacob Slaton <jslaton@protonmail.com>
2024-04-08 06:53:52 -0700

    Finish /card/{id}/image Endpoint

--- EdhutilApi/Controllers/card/{id}/image/CardIdImageController.cs
+++ EdhutilApi/Controllers/card/{id}/image/CardIdImageController.cs
@@ -10,16 +10,28 @@ namespace Edhutil.Api.Controllers
     public class CardIdImageController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get(Guid id)
+        public IActionResult Get
+        (
+            Guid id,
+            [FromQuery(Name="crop")] bool crop = false
+        )
         {
             Guid imageGuid = new();
             using (NpgsqlConnection connection = new("Host=localhost;Port=5432;Username=admin;Password=56a16c6a-8813-4204-bec6-5bb3739e356b;Database=edhutil"))
             {
-                string query = "SELECT uuid FROM cards WHERE uuid = @Uuid";
+                string query = "SELECT scryfall_id FROM cards WHERE uuid = @Uuid";
                 imageGuid = connection.Query<Guid>(query, new { Uuid = id }).FirstOrDefault();
             }
             string imageId = imageGuid.ToString().ToLower();
-            string url = $"https://cards.scryfall.io/large/front/{imageId[0]}/{imageId[1]}/{imageId}.jpg";
+            string url = string.Empty;
+            if (crop)
+            {
+                url = $"https://cards.scryfall.io/art_crop/front/{imageId[0]}/{imageId[1]}/{imageId}.jpg";
+            }
+            else
+            {
+                url = $"https://cards.scryfall.io/large/front/{imageId[0]}/{imageId[1]}/{imageId}.jpg";
+            }
             return Ok(url);
         }
     }
