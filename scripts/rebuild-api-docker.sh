4b75204029e70185284dcc6725c5e084d7dd3b2d
Jacob Slaton <jslaton@protonmail.com>
2024-04-21 21:02:36 -0700

    Update Rebuild Scripts

--- scripts/rebuild-api-docker.sh
+++ scripts/rebuild-api-docker.sh
@@ -1,5 +1,6 @@
 docker stop edhutil-api
 docker rm edhutil-api
+docker build ./Models -t edhutil-models:latest
 docker build ./EdhutilApi -t edhutil-api:latest
 docker run --name edhutil-api -d -p 5000:80 edhutil-api:latest
 
--- scripts/rebuild-discord-docker.sh
+++ scripts/rebuild-discord-docker.sh
@@ -1,5 +1,6 @@
 docker stop edhutil-discord
 docker rm edhutil-discord
+docker build ./Models -t edhutil-models:latest
 docker build ./DiscordBot -t edhutil-discord:latest
 docker run --name edhutil-discord -d edhutil-discord:latest
 
